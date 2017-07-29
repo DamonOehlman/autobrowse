a9c2ba8f4f05217c6eacb964900deaffa6ae6afb
Damon Oehlman <damon.oehlman@gmail.com>
2017-07-29 23:36:02 +1000

    limited safari support

---
+++ safari.js
@@ -0,0 +1,20 @@
+const { spawn } = require('child_process');
+const fs = require('fs');
+const Automator = require('./automator');
+// const optionPacks = require('./option-packs/chrome');
+
+const executables = [
+  // TODO: linux and windows executable locations
+  '/Applications/Safari.app/Contents/MacOS/Safari'
+];
+
+// detection
+exports.getExecutable = () => executables.filter(file => fs.existsSync(file))[0];
+
+//execution
+exports.exec = (uri, opts, callback) => {
+  const executable = exports.getExecutable();
+
+  const ps = spawn(`open -a safari ${uri}`)
+  callback(null, Automator.of(ps, () => rimraf(profile, () => {})));
+};
