7c086741911cc183846a8f69f80fa3920031ad7f
Damon Oehlman <damon.oehlman@gmail.com>
2017-07-29 23:52:43 +1000

    removed log line

--- safari.js
+++ safari.js
@@ -14,8 +14,6 @@ exports.getExecutable = () => executables.filter(file => fs.existsSync(file))[0]
 //execution
 exports.exec = (uri, opts, callback) => {
   const executable = exports.getExecutable();
-  console.log('starting safari');
-
   const ps = spawn('/usr/bin/open', ['-a', 'safari', uri]);
   callback(null, Automator.of(ps, () => {}));
 };
